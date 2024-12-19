e8b71fef9dedfe85669e7656a616754f2ee4729d
Dustin Delroy <dustindelroy@gmail.com>
2024-12-19 20:05:46 +0700

    Create prediction service

---
+++ app/Services/TypePredictionService.php
@@ -0,0 +1,24 @@
+<?php
+namespace App\Services;
+
+use Illuminate\Support\Facades\Http;
+
+class TypePredictionService
+{
+    public function predict($imgPath)
+    {
+        $fileContent = file_get_contents($imgPath); // Membaca isi file
+
+        $response = Http::withHeaders([
+            'Prediction-Key' => env('CUSTOM_VISIONS_TYPE_PREDICTION_KEY'),
+            'Content-Type' => 'application/octet-stream',
+        ])->withBody($fileContent, 'application/octet-stream')
+            ->post(env('CUSTOM_VISIONS_TYPE_PREDICTION_URL'));
+
+        if ($response->successful()) {
+            return $response->json();
+        }
+
+        throw new \Exception('Failed to fetch data: ' . $response->body());
+    }
+}
