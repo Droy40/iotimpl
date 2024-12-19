3f997c79428943a268e71c492f323a8a8dda697e
Dustin Delroy <dustindelroy@gmail.com>
2024-12-19 20:53:24 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
+use App\Http\Controllers\ApiPhotoController;
+use App\Http\Controllers\PhotoController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/photo');
 });
+
+Route::prefix('api')->group(function () {
+    Route::resource('photo', ApiPhotoController::class);
+
+});
+Route::Resource('photo', PhotoController::class);
